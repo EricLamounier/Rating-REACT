670eb7ff1405efbe8c252ac19ea02e746286bec9
EricLamounier <eric_lamounier@outlook.com>
2023-01-31 00:00:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import './App.css';
-import { BrowserRouter } from "react-router-dom";
-import Rotas from './Routes/Rotas'
-/*
-import Container from './Components/Container/Container';
-import Title from './Components/Title/Title';
-import Description from './Components/Description/Description';
+import { Link } from "react-router-dom";
+import Rotas from './Routes/Rotas';
 
-//images
-import starImg from './Images/icon-star.svg';
-*/
-/*
-      <Container>
-        <div id="starLogo">
-          <img src={starImg} alt="start icon"/>
-        </div>
-        <Title>Teste</Title>
-        <Description>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</Description>
-        <div id="rating">
-          <div data-i="1" class="box unselected">1</div>
-          <div data-i="2" class="box unselected">2</div>
-          <div data-i="3" class="box unselected">3</div>
-          <div data-i="4" class="box unselected">4</div>
-          <div data-i="5" class="box unselected">5</div>
-        </div>1
-      </Container>*/
 function App() {
   return (
     <main>
-      <BrowserRouter>
+      <div>
+        <nav>
+            <ul id="navigation">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/thanks">Thanks</Link>
+                </li>
+            </ul>
+        </nav>
         <Rotas/>
-      </BrowserRouter>
+    </div>
     </main>
   );
 }
--- src/Routes/Rotas.js
+++ src/Routes/Rotas.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import RatePage from '../Components/Pages/RatePage/RatePage';
 import ThanksPage from '../Components/Pages/ThanksPage/ThanksPage';
 
