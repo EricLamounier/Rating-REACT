76e05ebf53ffa19d6db570ac14a325386b754fa9
EricLamounier <eric_lamounier@outlook.com>
2023-02-04 17:12:20 -0300

    add readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import { Link } from "react-router-dom";
 import Rotas from './Routes/Rotas';
 
 function App() {
   return (
     <main>
-      <div>
-        <nav>
-            <ul id="navigation">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/thanks">Thanks</Link>
-                </li>
-            </ul>
-        </nav>
         <Rotas/>
-    </div>
     </main>
   );
 }
